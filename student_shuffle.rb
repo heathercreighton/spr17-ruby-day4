b7f9190edc7c6b57f0da7c681177d36fc634b147
Heather Creighton <heather.creighton@gmail.com>
2017-04-10 10:50:40 -0400

    paired students

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,9 +1,22 @@
 # Write a program that splits the class into teams of 2. If there's an odd number of 
 # students the last group should be a group of 3.
 
-students = ["Heather", "Niesha","Demetri", "Jelani", "Sean", "Finn"]
+students = ["Heather", "Niesha","Demetri", "Jelani", "Sean", "Finn","Michael"]
 
 #permanently reorders the student array
 students.shuffle! 
 
-puts students
+# puts students
+
+#Counts number of students the array
+count = 0
+
+num_students = students.length - 1
+
+until count == num_students
+
+		puts "#{students[count]} & #{students[count + 1]}"
+		count +=2
+
+end
+
